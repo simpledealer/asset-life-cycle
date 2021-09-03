7289d25644f3138d4524fb5e6013b6f0de0d7566
Rubin Apore <rubin.apore@gmail.com>
2021-09-03 02:00:52 +0000

    Clean lib for publishing

--- src/get-signed-url.js
+++ src/get-signed-url.js
@@ -1,9 +1,5 @@
-import { prop } from '@meltwater/phi'
 import createLogger from './util/logger'
-import getSalesCreditForm from './sales-credit-form/get-urls'
-export const assetTree = {
-  'sales-form-credit': getSalesCreditForm
-}
+import getKey from './get-urls'
 
 export const getSignedUrlPromise = ({
   operation,
@@ -18,8 +14,7 @@ export const getSignedUrlPromise = ({
 export default ({ s3Options, s3Client, assetName, version }) => options => async ({ ttl = 600 } = {}) => {
   const log = createLogger('getSignedUrl')
   const { bucket: Bucket } = s3Options
-  const keyFinder = prop(assetName, assetTree)
-  const key = keyFinder({ version, assetName })(options)
+  const key = getKey({ version, assetName })(options)
   log('Getting Signed Url', key)
   const operation = 'getObject'
   const params = { Key: key, Bucket, Expires: ttl }
--- src/is-asset-available.js
+++ src/is-asset-available.js
@@ -1,15 +1,11 @@
-import { prop } from '@meltwater/phi'
 import createLogger from './util/logger'
-import getSalesCreditForm from './sales-credit-form/get-urls'
-export const assetTree = {
-  'sales-form-credit': getSalesCreditForm
-}
+import getKey from './get-urls'
+
 export default ({ s3Options, s3Client, assetName, version }) => options => ({ ttl = 30000 } = {}) => {
   const log = createLogger('isAvailable')
   const { bucket: Bucket } = s3Options
-  const keyFinder = prop(assetName, assetTree)
-  const key = keyFinder({ version, assetName })(options)
-  return new Promise((resolve, reject) => {
+  const key = getKey({ version, assetName })(options)
+  return new Promise(resolve => {
     log('Checking', key)
     let [intervalPointer, timeOutPointer] = [null, null]
     const clearIntervals = () => {
@@ -17,18 +13,13 @@ export default ({ s3Options, s3Client, assetName, version }) => options => ({ tt
       if (timeOutPointer) clearInterval(timeOutPointer)
     }
     intervalPointer = setInterval(() => {
-      s3Client.headObject({
-        Bucket,
-        Key: key
-      }).promise()
+      s3Client.headObject({ Bucket, Key: key }).promise()
         .then(result => {
           log('Found', result)
           clearIntervals()
           resolve(true)
         })
-        .catch(() => {
-          log('Not found', key)
-        })
+        .catch(() => { log('Not found', key) })
     }, 1000)
     timeOutPointer = setTimeout(() => {
       // this means we could not get the item on time
