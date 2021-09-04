3203cd962e41bfbef9c043970763006ff5aa2ee2
Rubin Apore <rubin.apore@gmail.com>
2021-09-04 08:03:48 +0000

    Add prop to fxn

--- src/util/fxn.js
+++ src/util/fxn.js
@@ -2,3 +2,4 @@ export const compose = (...functions) => x => functions.reduceRight((acc, fn) =>
 export const includes = sub => mainStr => mainStr.includes(sub)
 export const not = val => !val
 export const noop = () => {}
+export const prop = (propName, obj) => obj[propName]
