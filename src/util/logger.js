976797c9b2d0405fc33383cab15e48f16dce1b38
Rubin Apore <rubin.apore@gmail.com>
2021-09-04 08:19:16 +0000

    Remove console.log

--- src/util/logger.js
+++ src/util/logger.js
@@ -1,5 +1,4 @@
 import debug from 'debug'
 import { name as applicationName } from '../../package.json'
 
-console.log('the name is ', applicationName)
 export default logName => debug(`${applicationName}:${logName}`)
