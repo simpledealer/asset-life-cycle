acecebd3cc4dc663fd82ab2f07778d8be77449fa
Rubin Apore <rubin.apore@gmail.com>
2021-09-11 11:40:56 +0000

    Add more logs for easy debugging

--- src/main.js
+++ src/main.js
@@ -15,13 +15,21 @@ export default ({
   const log = createLogger('lib:main')
   log('Getting Asset information')
   const stsCLient = new STS(s3Options)
+  log('STS client created')
   await stsCLient.getCallerIdentity().promise()
+  log('Identity verified')
   const s3Client = new S3(s3Options)
+  log('S3 client created')
   const version = await getVersion()
+  log('Asset Version', version)
   const isAvailable = createIsAssetAvailable({ s3Options, s3Client, version, assetName })(options)
+  log('IsAvailable Created')
   const getSignedUrl = createGetSignedUrl({ s3Options, s3Client, version, assetName })(options)
+  log('getSignedUrl Created')
   const repush = createRepushAsset({ s3Options, s3Client, version, assetName })(options)
+  log('repush created')
   const getKey = createGetKey({ s3Options, s3Client, version, assetName })(options)
+  log('getKey created')
   return {
     getKey,
     isAvailable,
